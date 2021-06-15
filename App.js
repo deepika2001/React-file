b019988dae418d76d3de32f54a73b75ed7733cb1
deepika2001 <60312891+deepika2001@users.noreply.github.com>
2021-06-15 10:14:21 +0530

    src file for calci

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ClearHistoryScreen from './ClearHistoryScreen.js';
+
+
+
+
+
+function App(){
+  return(
+    <div className="App">
+    <ClearHistoryScreen/>
+    
+    </div>
+  );
+}
+export default App;
---
+++ ClearHistoryScreen.js
@@ -0,0 +1,34 @@
+import React  from 'react';
+import './ClearHistoryStyle.css';
+import CalculationList from './CalculationList.js';
+import './CalculationList.css';
+
+
+
+
+class ClearHistoryScreen extends React.Component{
+
+       render() {
+            return(
+              <div>
+                     <div className='container'>
+                     <CalculationList> </CalculationList>
+                     </div>
+                     <button>ClearHistory</button>
+                   
+                     <div>
+                      <i class="fas fa-arrow-alt-circle-left"></i>
+                     </div>
+                
+                     </div>
+                     
+                     )
+                
+        }
+      
+        
+};
+
+
+
+export default ClearHistoryScreen;
